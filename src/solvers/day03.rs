cedbb456ec8121ff9444ddbab25c462a2db8f8be
Brad Campbell <bcampbell1120@gmail.com>
2020-12-03 16:39:29 -0500

    More generic solution for day03-1

--- src/solvers/day03.rs
+++ src/solvers/day03.rs
@@ -2,34 +2,30 @@ use std::io::{BufReader, BufRead};
 use std::fs::File;
 
 pub fn solve(input: BufReader<File>) {
-    /*
-    /// Count the trees encountered for a given x and y hop value
-    fn count_trees(lines: Vec<String>, x: usize, y: usize) -> usize {
-        lines.iter()
-            .enumerate()
-            .filter_map(|(i, e)| {
-                if i % y == 0 { Some(e) } else { None }
-            })
-            .fold((0, 0), |acc, line| {
-                if line.as_bytes()[i % line.len()] == '#' as u8 { (acc + 1, i + x) } else { (acc, i + x) }
-            })
-    }
-    */
+
+
+    let lines = input.lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<_>>();
 
     // Solve part 1
-    let result_1 = input.lines()
-        .fold((0, 0), |acc, line| {
-            let line = line.unwrap();
-            let index = acc.0 % line.len();
-            let result = match line.as_bytes()[index] == '#' as u8 {
-                true => acc.1 + 1,
-                false => acc.1
-            };
-
-            (index + 3, result)
-        }).1;
+    let result_1 = count_trees(lines, 3, 1);
 
     //let lines = input.lines().map(|line| line.unwrap()).collect::<Vec<_>>();
 
     println!("[+] Day03-1: {}", result_1);
+}
+
+/// Count the trees encountered for a given x and y hop value
+fn count_trees(lines: Vec<String>, x: usize, y: usize) -> usize {
+    lines.iter()
+        .fold((0, 0), |(index, counter), line| {
+            let index = index % line.len();
+            let result = match line.as_bytes()[index] == '#' as u8 {
+                true => counter + 1,
+                false => counter
+            };
+
+            (index + x, result)
+        }).1
 }
