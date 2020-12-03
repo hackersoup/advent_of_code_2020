d3cfb3bdc3794c9a81587dc1d749e62f3e427bb5
Brad Campbell <bcampbell1120@gmail.com>
2020-12-03 16:50:04 -0500

    Day03 part 2 solution

--- src/solvers/day03.rs
+++ src/solvers/day03.rs
@@ -4,22 +4,31 @@ use std::fs::File;
 pub fn solve(input: BufReader<File>) {
 
 
-    let lines = input.lines()
+    let lines = &input.lines()
         .map(|line| line.unwrap())
         .collect::<Vec<_>>();
 
     // Solve part 1
     let result_1 = count_trees(lines, 3, 1);
 
-    //let lines = input.lines().map(|line| line.unwrap()).collect::<Vec<_>>();
+    let result_2 = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)].iter()
+        .fold(1, |acc, (x, y)| {
+            acc * count_trees(&lines, *x as usize, *y as usize)
+        });
 
     println!("[+] Day03-1: {}", result_1);
+    println!("[+] Day03-2: {}", result_2);
 }
 
 /// Count the trees encountered for a given x and y hop value
-fn count_trees(lines: Vec<String>, x: usize, y: usize) -> usize {
+fn count_trees(lines: &Vec<String>, x: usize, y: usize) -> usize {
     lines.iter()
-        .fold((0, 0), |(index, counter), line| {
+        .enumerate()
+        .fold((0, 0), |(index, counter), (linenum, line)| {
+            if linenum % y != 0 {
+                // Skip this line
+                return (index, counter)
+            }
             let index = index % line.len();
             let result = match line.as_bytes()[index] == '#' as u8 {
                 true => counter + 1,
